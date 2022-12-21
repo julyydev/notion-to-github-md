fe1745957bafab555e0cca3257b5cec936011226
Jaeyoung Kwak <66120479+julyydev@users.noreply.github.com>
2022-12-21 20:58:16 +0900

    [FEAT] implement createMDwithChildrenBlock

---
+++ src/services/notion/createMDwithChildrenBlock.ts
@@ -0,0 +1,19 @@
+import { notion } from '../../config/notion';
+import { createMDwithBlock } from './createMDwithBlock';
+
+export const createMDwithChildrenBlock = async (
+    block_id: string,
+    depth: number,
+) => {
+    const response = await notion.blocks.children.list({ block_id: block_id });
+    const mds: string[] = [];
+    mds.push('    '.repeat(depth));
+    for (const block of response.results) {
+        if ('type' in block) {
+            const md = await createMDwithBlock(block, depth);
+            md !== undefined && mds.push(md);
+        }
+    }
+
+    return mds.join('');
+};
