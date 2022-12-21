99fc2ada62d466cd709aa043aeadf8f4a4ac366f
Jaeyoung Kwak <66120479+julyydev@users.noreply.github.com>
2022-12-21 20:57:12 +0900

    [FEAT] implement getEditedPageList

---
+++ src/services/notion/getEditedPageList.ts
@@ -0,0 +1,36 @@
+import { notion } from '../../config/notion';
+import { PageStatus } from '../../constants/pageStatus';
+
+export const getEditedPageList = async () => {
+    const editedPageList: { id: string; title: string }[] = [];
+
+    const response = await notion.databases.query({
+        database_id: '2d03181f-721b-410a-a832-0192af606f35',
+    });
+
+    response.results.forEach(page => {
+        if ('properties' in page) {
+            if ('상태' in page.properties) {
+                if (page.properties.상태.type === 'status') {
+                    if (
+                        page.properties.상태.status?.name ===
+                            PageStatus.EDITED &&
+                        page.properties.제목.type === 'title'
+                    ) {
+                        editedPageList.push({
+                            id: page.id,
+                            title: page.properties.제목.title[0].plain_text.replace(
+                                / /g,
+                                '-',
+                            ),
+                        });
+                    }
+                }
+            }
+        }
+    });
+
+    console.log(editedPageList.length + '건의 업데이트가 발견되었습니다.');
+
+    return editedPageList;
+};
