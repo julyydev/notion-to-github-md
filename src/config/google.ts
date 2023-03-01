3524b706795b16f6af9b8c89b5c20613b425a586
Jaeyoung Kwak <66120479+julyydev@users.noreply.github.com>
2023-03-01 18:17:52 +0900

    [Feat] 구글 인증 방식 service account로 변경

--- src/config/google.ts
+++ src/config/google.ts
@@ -3,15 +3,27 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const googleClient = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URL,
-);
+const config = {
+    type: 'service_account',
+    project_id: 'blog-image-management',
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    client_email: 'bim-server@blog-image-management.iam.gserviceaccount.com',
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url:
+        'https://www.googleapis.com/robot/v1/metadata/x509/bim-server%40blog-image-management.iam.gserviceaccount.com',
+};
 
-// TODO: refresh token 재발급 및 저장 로직 필요
-googleClient.setCredentials({
-    refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+const googleAuth = new google.auth.GoogleAuth({
+    scopes: ['https://www.googleapis.com/auth/drive'],
 });
 
-export const google_drive = google.drive({ version: 'v3', auth: googleClient });
+const auth = googleAuth.fromJSON(config);
+
+export const google_drive = google.drive({
+    version: 'v3',
+    auth: auth,
+});
